3b52c4d9893dbbe6caeaa4eaaece3b86ce029998
Goro_sb71@stud.msk.itstep.org <Goro_sb71@stud.msk.itstep.org>
2022-06-08 17:41:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using System.Text;
+
+
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+      
+
+        static void Main(string[] args)
+        {
+           
+            IntPtr hWnd;
+            hWnd = WinApi.GetForegroundWindow();
+            IntPtr count = hWnd;
+            StringBuilder WindowTEXT = new StringBuilder(150);
+
+
+            bool Admin = WinApi.IsUserAnAdmin();
+            Console.WriteLine($"Программма запущена от имени администратора : {Admin}");
+
+            while (true)
+            {
+                
+                 hWnd = WinApi.GetForegroundWindow();
+                if (hWnd != count)
+                {
+
+                    WinApi.GetWindowText(hWnd, WindowTEXT, WindowTEXT.Capacity);
+
+                    if (WindowTEXT.Length > 0)
+                    {
+                        Console.WriteLine($" Передний план {WindowTEXT}");
+                    }
+                    count = hWnd;
+                }
+            }
+        }  // на коментарии сил уже не осталось 
+    }
+
+    
+}
---
+++ ConsoleApp3/WinApi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices; 
+
+namespace ConsoleApp3
+{
+    internal class WinApi
+    {
+
+
+        [DllImport("shell32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool IsUserAnAdmin();
+
+
+        [DllImport("user32.dll ")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowText",
+    ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpWindowText, int nMaxCount);
+    }
+}
